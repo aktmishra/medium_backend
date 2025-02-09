0c88b5e622118dc9c6dd6e72ec99a8d81cdf5443
Ankit Mishra <aktmishra72@gmail.com>
2025-02-09 20:09:11 +0530

    initialized schema and migrate database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+   
+
+   return c.text('Hello Hono!')
 })
 
 export default app
